3dfad433b8a80c6abe4f121d89bedf672294e52a
DivaLapper <63549227+DivaLapper@users.noreply.github.com>
2020-04-19 12:34:28 +0530

    Changes made to package.json

--- index.js
+++ index.js
@@ -5,13 +5,4 @@ app.get('/', function (req, res) {
 });
 app.listen(3000, function () {
   console.log('listening on port 3000!');
-
-
-
 });
-app.listen(3001, function () {
-    console.log('listening on port 300!');
-  
-  
-    
-  });
