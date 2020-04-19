817853f86ccccd8758b4ff49df63da86ef8e5a66
DivaLapper <63549227+DivaLapper@users.noreply.github.com>
2020-04-19 12:18:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ app.listen(3000, function () {
   console.log('listening on port 3000!');
 
 
-  
+
 });
+app.listen(3001, function () {
+    console.log('listening on port 300!');
+  
+  
+    
+  });
